df1727f55065eee9626ab272a66b55f11c5db105
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-29 02:35:18 +0100

    Add products and categories models, migrations, factories
    Introduce `Product` and `Category` models with relationships and factories to enable data generation for testing and seeding. Added corresponding database migrations for `products` and `categories` tables, and updated the `DatabaseSeeder` to populate sample data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +21,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Category::factory(10)->create();
+        Product::factory(100)->create();
     }
 }
