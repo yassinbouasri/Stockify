568b524bfe0af2730ec0b402497b798a1adeb934
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-31 18:42:23 +0100

    Dirty state added to the edit form

--- resources/views/livewire/create-product.blade.php
+++ resources/views/livewire/create-product.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class=" mx-4 my-4">
             <x-label class="p-1">Category:</x-label>
-            <select class="w-1/4 p-1 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" wire:model.defer="form.category_id">
+            <select class="w-1/4 p-1 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" wire:model="form.category_id">
                 @foreach($this->categories as $category)
                     <option value="{{(int) $category->id}}">{{$category->name}}</option>
                 @endforeach
--- resources/views/livewire/update-product.blade.php
+++ resources/views/livewire/update-product.blade.php
@@ -1,21 +1,21 @@
 <div class="grid grid-cols-2 gap-8">
     <div>
         <div class="mb-4 text-center text-xl">
-            <h3>Edit Product ID:({{ $this->form->id }})</h3>
+            <h3 class="dark:text-gray-300">Edit Product ID:({{ $this->form->id }})</h3>
         </div>
         <form wire:submit="save">
             <div class=" mx-4 my-4">
-                <x-label class="p-1">Product Name:</x-label>
+                <x-label wire:target="form.name" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1 ">Product Name:<span wire:dirty wire:target="form.name">*</span></x-label>
                 <x-input class="w-1/2 p-1"
                          wire:model="form.name"
                          autofocus
                 />
             </div>
             <div class=" mx-4 my-4">
-                <x-label class="p-1">Category:</x-label>
+                <x-label wire:target="form.category_id" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1">Category:<span wire:dirty wire:target="form.category_id">*</span></x-label>
                 <select
                     class="w-1/2 p-1 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-                    wire:model.defer="form.category_id">
+                    wire:model="form.category_id">
                     @foreach($this->categories as $category)
                         <option value="{{(int) $category->id}}">{{$category->name}}</option>
                     @endforeach
@@ -23,27 +23,26 @@
             </div>
 
             <div class=" mx-4 my-4">
-                <x-label class="p-1">SKU:</x-label>
+                <x-label wire:target="form.sku" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1">SKU:<span wire:dirty wire:target="form.sku">*</span></x-label>
                 <x-input class="w-1/2 p-1"
                          wire:model="form.sku"
                 />
             </div>
             <div class=" mx-4 my-4">
-                <x-label class="p-1">Price:</x-label>
+                <x-label wire:target="form.price" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1">Price:<span wire:dirty wire:target="form.price">*</span></x-label>
                 <x-input type="number" class="w-1/2 p-1"
                          wire:model="form.price"
                 />
             </div>
             <div class=" mx-4 my-4">
-                <x-label class="p-1">Description:</x-label>
-
+                <x-label wire:target="form.description" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1">Description:<span wire:dirty wire:target="form.description">*</span></x-label>
                 <textarea wire:model="form.description"
                           class="font-medium text-sm text-gray-700 dark:text-gray-300 dark:bg-gray-900 dark:border-gray-600 w-1/2 p-4">
 
             </textarea>
             </div>
             <div class=" mx-4 my-4">
-                <x-label class="p-1">Image:</x-label>
+                <x-label wire:target="form.photo" wire:dirty.class="text-orange-400 dark:text-orange-400" class="p-1">Image:<span wire:dirty wire:target="form.photo">*</span></x-label>
 
                 <x-input type="file" class="w-1/2 p-1"
                          wire:model="form.photo"
