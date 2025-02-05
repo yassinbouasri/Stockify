56edf473656694909aa82e80b8902e0dc10e9e50
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-02-05 22:12:21 +0100

    feat: close search results on outside click
    - Add event listener to close search results when clicking outside the component
    - Update UI state ($show) to false on outside interaction
    - Improve UX by ensuring results disappear when not focused

--- resources/views/livewire/search-customer.blade.php
+++ resources/views/livewire/search-customer.blade.php
@@ -12,11 +12,13 @@
     @if($this->show)
 
         <div
-            class="ml-4 absolute border border-indigo-700 rounded-lg dark:bg-gray-700 max-h-60 overflow-y-auto shadow-lg bg-gray-100 bg-opacity-50 "
+            wire:transition.100ms
+            class="ml-4 absolute border border-indigo-700 rounded-lg dark:bg-gray-700 max-h-60 overflow-y-auto shadow-lg bg-gray-100 bg-opacity-50 dark:bg-opacity-50"
         >
 
             @foreach($this->customers as $customer)
                 <a
+
                     wire:click="selectCustomer({{ $customer->id }})"
                    href="#"
                    class="dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600 ">
