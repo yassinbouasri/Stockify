767ba05e2cf051295f8d4471dd9372c2d06621e7
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-02-16 01:31:57 +0100

    show low stocks in red

--- resources/views/livewire/stock-quantity.blade.php
+++ resources/views/livewire/stock-quantity.blade.php
@@ -1,27 +1,30 @@
 <div
-    x-data="{ editing: false }"
-    x-on:click="editing = true"
-    class="relative"
+        x-data="{ editing: false }"
+        x-on:click="editing = true"
+        class="relative"
 >
-    <span x-show="!editing" class="cursor-pointer">
-        {{ $quantity }}
-    </span>
+
+            <span x-show="!editing" class="cursor-pointer" >
+                {{ $quantity }}
+            </span>
+
+
 
     <div x-show="editing" class="absolute top-0 left-0">
         <input
-            autofocuse
-            x-cloak
-            type="number"
-            wire:model.live="quantity"
-            wire:change="updateQuantity"
-            x-on:keydown.enter="editing = false"
-            x-on:keydown.escape="editing = false"
-            x-on:keydown.arrow-up="quantity++"
-            x-on:keydown.arrow-down="quantity--"
-            x-on:blur="editing = false"
-            class="w-20 h-6 mt-0 mb-5 border rounded shadow-sm text-sm bg dark:bg-gray-700"
-            autofocus
-            min="0"
+                autofocuse
+                x-cloak
+                type="number"
+                wire:model.live="quantity"
+                wire:change="updateQuantity"
+                x-on:keydown.enter="editing = false"
+                x-on:keydown.escape="editing = false"
+                x-on:keydown.arrow-up="quantity++"
+                x-on:keydown.arrow-down="quantity--"
+                x-on:blur="editing = false"
+                class="w-20 h-6 mt-0 mb-5 border rounded shadow-sm text-sm bg dark:bg-gray-700"
+                autofocus
+                min="0"
         >
     </div>
 </div>
